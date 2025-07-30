1c8406686617aa0b32948ac9053ca57a0786b9f9
Zyan Karim <zyan.karim@gmail.com>
2025-07-29 22:32:34 -0500

    added requirements.txt and main.py along with file upload system for the dashboard

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import json
+import os
+
+st.set_page_config(page_title='Simple Finance App', page_icon='💲', layout='wide')
+
+def load_transactions(file):
+    try:
+        df = pd.read_csv(file)
+        st.write(df)
+        return df
+    except Exception as e:
+        st.error(f'Error processing file: {str(e)}')
+        return None
+
+def main():
+    st.title('Simple Finance Dashboard')
+
+    uploaded_file = st.file_uploader('Upload your transaction csv file', type=['csv'])  
+
+    if uploaded_file is not None:
+        df = load_transactions(uploaded_file)
+    
+
+if __name__ == '__main__':
+    main()
